3e518cc4a21f4fabdec3e3834b22bb160f148003
JackandM-git <malamudyaakov@gmail.com>
2023-12-21 22:47:11 -0600

    Edited some names

--- Scenes/First_World/world_1.gd
+++ Scenes/First_World/world_1.gd
@@ -1,6 +1,5 @@
 extends MeshInstance3D
 
-var axis = Vector3(0,1,0)
 
 ##this is the speed of rotation
 @export var rotate_amount = 3
@@ -10,5 +9,5 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		rotate_y(rotate_amount * delta)
 	
-	if Input.is_action_pressed("move_left"):
-		rotate_y(-rotate_amount * delta)
+	elif Input.is_action_pressed("move_left"):
+		rotate_y(-rotate_amount * delta) 
